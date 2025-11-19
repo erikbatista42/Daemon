e3bb3530f2831a27ed8eb9b356a96aa6d3ac6b99
erikbatista42 <erikbatista42@gmail.com>
2025-11-19 02:01:49 -0500

    working w langchain

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Daemon
 A centralized RAG pipeline that aggregates isolated data streams with integrations into a queryable knowledge base.
+
+**MVP Tech Stack**:
+- Python
+- LangChain
+- ChromaDB (Local)
+- Ollama (Local LLM)
+- Slack SDK
+
+
---
+++ main.py
@@ -0,0 +1,29 @@
+# pip install -qU "langchain[anthropic]" to call the model
+
+from langchain.agents import create_agent
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+agent = create_agent(
+    model=ChatOpenAI(model_name="gpt-5-nano",
+    api_key=os.environ.get("OPENAI_API_KEY")),
+    tools=[get_weather],
+    system_prompt="You are a helpful assistant",
+)
+
+# Run the agent
+invoked = agent.invoke(
+    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+)
+
+for message in invoked["messages"]:
+    print(f"\n[{message.type.upper()}]: {message.content}")
